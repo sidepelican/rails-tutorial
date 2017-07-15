f88dfca1f7d75399913d7b63807932cb4f91b6f8
sidepelican <side.junktown@gmail.com>
2017-07-15 13:34:34 +0000

    create login view and simple auth

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # login success
+    else
+      # on error
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
