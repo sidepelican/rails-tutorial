08d8c42f9703db9b9fa8b9bf21cd8ca28cb0ccfe
sidepelican <side.junktown@gmail.com>
2017-07-20 06:54:14 +0000

    add password reset tests

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -43,7 +43,7 @@ class User < ApplicationRecord
   
   # アカウントを有効にする
   def activate
-    update_attribute(activated: true, activated_at: Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # 有効化用のメールを送信する
@@ -54,12 +54,16 @@ class User < ApplicationRecord
   # パスワード再設定の属性を設定する
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
   end
 
   # パスワード再設定のメールを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+  
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
   
   private
