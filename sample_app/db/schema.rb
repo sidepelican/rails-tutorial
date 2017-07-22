5a4c15fd36c97a7a4045cf98b9cfa74c39ae51eb
sidepelican <side.junktown@gmail.com>
2017-07-22 09:31:37 +0000

    show microposts

--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20170722085846) do
     t.string   "remember_digest"
     t.boolean  "admin",             default: false
     t.string   "activation_digest"
-    t.boolean  "activated"
+    t.boolean  "activated",         default: false
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
